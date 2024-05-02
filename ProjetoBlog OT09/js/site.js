2649e1e2c2d4e27d58e0ea3cea1c947bd11c647b
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2024-05-02 19:51:53 -0300

    Correção e finalização das OT's 08 / 09

--- ProjetoBlog OT09/js/site.js
+++ ProjetoBlog OT09/js/site.js
@@ -1,5 +1,11 @@
 function validaInscricao() {
     
+    if(document.frminscricao.txtnome.value == ""){
+        alert("Preencha o campo Nome.");
+        document.frminscricao.txtnome.focus();
+        return false;
+    }
+    
     var fone = document.getElementById('fone').value;
     var foneRegex = /^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$/; // Expressão regular corrigida
 
@@ -15,6 +21,27 @@ function validaInscricao() {
         alert("CEP válido.");
     } else {
         alert("CEP inválido. Por favor, insira um CEP no formato XXXXX-XXX.");
+    }
+
+    if (!document.querySelector('input[name="radiosexo"]:checked')) {
+        alert("Por favor, selecione uma opção.");
+        return false;
+    }
+
+    var email = document.getElementById('email').value;
+
+    // Expressão regular para validar o formato do email
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Verifica se o email corresponde à expressão regular
+    if (!emailRegex.test(email)) {
+    alert("Por favor, insira um email válido.");
+    return false;
+    }
+
+    if (!document.getElementById('checkbox').checked) {
+        alert("Por favor, selecione a opção.");
+        return false;
     }
 
 }
